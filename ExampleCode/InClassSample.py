88f221439d55ea4a38c32cb1d4281d01f6ad64d4
Albert Lionelle <Albert.Lionelle@colostate.edu>
2021-08-31 13:32:31 -0600

    update slides for lecture

--- ExampleCode/InClassSample.py
+++ ExampleCode/InClassSample.py
@@ -1,9 +1,16 @@
 
-answer = input("Who was the world's first programmer?")
+def longerFunc(person, adj, code):
+    print("Hello " + person)
+    print("Cryptography is the " + adj)
+    code = code + code / 2
+    print("The hidden number is: ", int(code))
 
-print(answer + " was the world's first programmer.")
+longerFunc("Valentine", "bombe", 10.0)
+print()
+print()
 
-
-
-
-
+###
+# computer = "Bombe Machine"  # assignment of string to variable
+# code = 10  # whole number - called "int"
+# formula = (code * 3/2) - 2.1
+# print("The code to the " + computer + " is ", formula)
