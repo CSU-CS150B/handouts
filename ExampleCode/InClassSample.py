a5c00860c2ca251c1972ac6b10d3b794754ab221
Albert Lionelle <Albert.Lionelle@colostate.edu>
2021-09-23 15:30:55 -0600

    update with in class modifications.

--- ExampleCode/InClassSample.py
+++ ExampleCode/InClassSample.py
@@ -1,31 +1,49 @@
 
-def division(dividend, divisor):
-    #TODO
-    whole = dividend // divisor ## hmmm
-    remainder =  dividend  % divisor #hmmm hmmm
-    return "Answer: {} with remainder {}".format(whole, remainder)
 
-print("TEST", division(250, 6))
-print("TEST", division(1,  2))
-
-
-
-
-
+# Write a line of code that returns true if one is “Rock”
+# and two is “Scissors” (now finish the game)
+def check_single(one, two):
+    if "Rock" == one  and "Scissors" == two:
+        return True
+    elif "Scissors" == one and "Paper" == two:
+        return True
+    elif "Paper" == one  and "Rock" == two:
+        return True
+    return False
 
 
-def shift(char, key):
-    pass
-    # TODO
 
-print(shift('A', 2))
-print(shift('A', 3))
 
-def simple_cipher(message, key):
-    index = 0
-    encrypted = ""
-    # TODO
-    return encrypted
 
-print("TESTING", simple_cipher("Ada", 3))
-print("TESTING", simple_cipher("20", 2))
+
+
+print("TESTING", check_single("Rock", "Scissors"),
+      check_single("Rock", "Bomb"),
+      check_single("Scissors", "Paper"),
+      check_single("Paper", "Rock"))
+
+
+
+
+
+
+
+
+
+favorite_companion = input("Who is your favorite companion? ")
+favorite_companion2 = input("Who is the clever one? ")
+if (favorite_companion == "Amy" or favorite_companion == "Martha") and favorite_companion2 == "Clara":
+    print("they are all good!")
+else:
+    print("Did you forget Clara?")
+
+
+def rock_paper_scissors():
+    winner = False
+    while not winner:
+        player1 = input("Rock, Paper, or Scissors? ")
+        player2 = input("Rock, Paper, or Scissors? ")
+        p1_win = check_single(player1, player2)
+        p2_win = check_single(player2, player1)
+        ## todo
+
