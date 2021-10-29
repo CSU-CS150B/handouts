6e57ee63ff65357227ac99528e9fb7de19e78e4f
Albert Lionelle <Albert.Lionelle@colostate.edu>
2021-10-28 19:56:17 -0600

    updates to inclass code  example

--- ExampleCode/InClassDictionary.py
+++ ExampleCode/InClassDictionary.py
@@ -1,19 +1,55 @@
 
 
 
+#create a dictionary with some 'assignments' in it, 
+my_grades =  {"a1": 3, "a2": 4, "p1": 0, "p3":"n/a"}
+
+## print some out
+print(my_grades["a1"])
+print(my_grades["p1"])
+
+# add another
+my_grades["another"] = 10
+
+# print all out
+print(my_grades)
+
+for assignment, grade in my_grades.items():
+    print(assignment, grade)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def update_grade(grades):
     assignment = input("Which assignment do you want to update? ")
     grade = input("What grade would you like? ")
-    grades[assignment] = grade # maybe turn to 
+    grades[assignment] = grade
 
 def run():
-    grades = {} # initialize dictionary
+    grades = {}
     print("Welcome to the grade book.")
     action = input("what would you like to do? ")
     while(action != "exit") :
         if action == "update":
             update_grade(grades)
+        if action == "print":
+            print(grades)
         action = input("what would you like to do? ")
 
-run()
+#run()
+
+
+
+
+
+ 
