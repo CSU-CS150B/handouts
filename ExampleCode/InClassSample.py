469239fff4a697c589e7886552698279f7aca4d2
Albert Lionelle <Albert.Lionelle@colostate.edu>
2021-11-09 19:54:12 -0700

    Inclass review

--- ExampleCode/InClassSample.py
+++ ExampleCode/InClassSample.py
@@ -1,10 +1,35 @@
+import csv
+
+lst = []
+
+with open("mobs.csv") as csvfile:
+    mobs = csv.reader(csvfile)
+    print(mobs)
+
+    for row in mobs:
+        lst.append(row)
+#        print(mob) #list including the header! (just a line)
+
+filtered = []
+for row in lst:
+    if 'zombie' in row:
+        filtered.append(float(row[1]))
+
+print(sum(filtered)/len(filtered))
 
 
-def myfun(lst):
-    small = min(lst)
-    large = max(lst)
-    avg = sum(lst) / len(lst)
-    return "Max: {}, Min:{}, Avg:{}".format(small, large, avg)
+dct = {'val1':10, 'val2':30}
 
-print(myfun([10, 20, 128, 74]))
-print(myfun([1, 20, 120, 70]))
+for val,value in  dct.items():
+    print(val,value)
+
+
+def myfunc(val = 10):
+    print("this is my function")
+    return val * 10 # ok silly
+
+def myfunc2():
+    print("this is the first  one ")
+    print(myfunc(12))
+
+myfunc2()
