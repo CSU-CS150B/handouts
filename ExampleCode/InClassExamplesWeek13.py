4722373f0bfeca34664461a7e65c1cd7265e442c
Albert Lionelle <Albert.Lionelle@colostate.edu>
2021-11-16 14:51:49 -0700

    updated with inclass example

--- ExampleCode/InClassExamplesWeek13.py
+++ ExampleCode/InClassExamplesWeek13.py
@@ -2,18 +2,22 @@
 lst = [12, 22, 13, 12, 15, 42, 21, 19]
 new_list = []
 for val in lst: 
-    pass
-    # Student code here
-print(new_list)
+    if val % 2 == 0:
+        new_list.append(val)
 
+print(new_list)
+tmp = [i for i in new_list if i % 2 == 0]
+print(tmp)
 
 lst = [["A-", "14.23"],
     ["just B", "12.12"],
     ["A+", "16.00"],
-    ["A", "15.00"]
+    ["A", "15.00"],
     ["F", "0"]]
 
+tmp2 = [float(ls[1]) for ls in lst if "A" in ls[0]]
+print(tmp2)
 # the new list built would return
-[14.23, 16.00, 15.00]
+#[14.23, 16.00, 15.00]
 ### use List Comprehensions! 
 ### hint in operator helps
