13b4afa21bf36e7f9b57b00619f15630aa84e5b4
Albert Lionelle <lionelle@gmail.com>
2021-11-15 19:34:07 -0700

    update for wed for lecture

---
+++ ExampleCode/InClassExamplesWeek13.py
@@ -0,0 +1,19 @@
+
+lst = [12, 22, 13, 12, 15, 42, 21, 19]
+new_list = []
+for val in lst: 
+    pass
+    # Student code here
+print(new_list)
+
+
+lst = [["A-", "14.23"],
+    ["just B", "12.12"],
+    ["A+", "16.00"],
+    ["A", "15.00"]
+    ["F", "0"]]
+
+# the new list built would return
+[14.23, 16.00, 15.00]
+### use List Comprehensions! 
+### hint in operator helps
