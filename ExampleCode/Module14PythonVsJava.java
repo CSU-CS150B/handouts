d352144cb50e81798dd97c138dee53fb91afa9f9
Albert Lionelle <Albert.Lionelle@colostate.edu>
2021-12-01 11:09:03 -0700

    update errors in java code transition

---
+++ ExampleCode/Module14PythonVsJava.java
@@ -0,0 +1,48 @@
+public class Module14PythonVsJava {
+
+    public static void myCoolMethod() {
+        System.out.println("Done");
+    }
+
+    public static void myCoolMethod2() {
+        System.out.println("Done2");
+    }
+
+    // entry point!
+    public static void main(String[] args) {
+        myCoolMethod();
+        myCoolMethod2();
+    }
+
+    public static void typeExample() {
+        int x = 10;
+        //x = 10.5; // ERROR!
+        float y = 10.5f;
+    }
+
+    public static String for_test(String letter, int times) {
+        String result = "";
+        for(int i = 0; i < times; i++) {
+            result += letter;
+        }
+        return result;
+    }
+
+
+
+    public static int if_test(int value, int value2) {
+        int rtn = 0; // going to be -1, 0, 1
+        if(value < value2) {
+            rtn = -1;
+        }
+        else if(value > value2) {
+           rtn = 1;
+        }
+        return rtn; // else assumed by 0
+    }
+
+
+
+}
+
+
