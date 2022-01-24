532cba5dfa2629c7bd092e7c57a6f4b43872320e
Albert Lionelle <albert.lionelle@colostate.edu>
2022-01-23 18:45:14 -0700

    updated with inclass activity

---
+++ ExampleCode/01IntroToProgramming/InclassActivity.py
@@ -0,0 +1,36 @@
+
+# This is an inclass activity.
+
+# For programming every line of code that isn't a comment
+# Gets executed one at a time
+# Eventually, you will learn how to block
+# these lines into resuable code snippets (called functions)
+# but for now, we will just run every line, one at a time. 
+
+# STEP 1: create variables, one for each person at the table
+# Store their names in the variables
+# example:
+person0 = "Samus Aran"
+
+
+# Step 2: Write a variables that store everyones
+# favorite number
+fav0 = 22
+
+
+# Step 3: write a print statements that outputs
+# "name's favorite number is number"
+# you may need to use the str(value) function!, example str(fav0)
+
+print()
+print()
+print("..Interactive Program Start..")
+# Step 4: Get an input from a client asking their name
+
+# Step 5: Get an input from a client asking their favorite number
+
+# Step 6: print out name's favorite number is number
+
+
+
+
